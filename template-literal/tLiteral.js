8e25d4c00cea7d4376fa9f526e6a20e8c09b0a76
zeerob440 <zeerob440@tutanota.com>
2025-12-26 14:42:49 -0500

    fixed: tLiteral.js linked to template-literal/index.html

--- template-literal/tLiteral.js
+++ template-literal/tLiteral.js
@@ -2,7 +2,9 @@
 
 let bestCity = 'Miami';
 
+/*Template literals are like Python f-strings, but f' is replaced by ``, and {} is replaced
+by ${}*/
 let popup = `The best city on the planet is ${bestCity} dale!`;
 
-console.log(popup); // outputs to console
-alert(popup); // outputs via popup to user
+console.log(popup); // outputs to console.
+alert(popup); // outputs via popup to user.
