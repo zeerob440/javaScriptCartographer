bee5ff345468478f278975068bf239d496c684c2
zeerob440 <zeerob440@tutanota.com>
2025-12-26 15:09:52 -0500

    fixed: grammar in tLiteral.js

--- template-literal/tLiteral.js
+++ template-literal/tLiteral.js
@@ -4,7 +4,7 @@ let bestCity = 'Miami';
 
 /*Template literals are like Python f-strings, but f' is replaced by ``, and {} is replaced
 by ${}*/
-let popup = `The best city on the planet is ${bestCity} dale!`;
+let popup = `The best city on the planet is ${bestCity}, dale!`;
 
 console.log(popup); // outputs to console.
 alert(popup); // outputs via popup to user.
