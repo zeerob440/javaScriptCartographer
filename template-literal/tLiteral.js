e82e8c236f4f2e43db77e305575b628d763dfea1
zeerob440 <zeerob440@tutanota.com>
2025-12-26 14:39:10 -0500

    docs: tLiteral.js written

---
+++ template-literal/tLiteral.js
@@ -0,0 +1,8 @@
+// a short program that demonstrates a Template Literal
+
+let bestCity = 'Miami';
+
+let popup = `The best city on the planet is ${bestCity} dale!`;
+
+console.log(popup); // outputs to console
+alert(popup); // outputs via popup to user
