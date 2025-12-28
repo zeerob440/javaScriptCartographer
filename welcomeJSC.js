7f9de81902fdc39a213f00cfad03a2291b07d12f
MU-TH-ER <zeerob440@tutanota.com>
2025-12-28 15:37:10 -0500

    feat: welcomeJSC added to root/index.html

---
+++ welcomeJSC.js
@@ -0,0 +1,8 @@
+//basic Javascript welcome program.
+
+document.getElementById("welcomeJSC").addEventListener("click", welcomeJSC);
+
+function welcomeJSC()
+    {alert("Welcome to JavaScriptCartographer!");
+
+    }
