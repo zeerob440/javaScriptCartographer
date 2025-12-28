32b7384ef7ba0392c12ed7190b0b29e0901e1327
MU-TH-ER <zeerob440@tutanota.com>
2025-12-28 16:44:02 -0500

    feat: added a line break in alert()

--- output/alert.js
+++ output/alert.js
@@ -2,4 +2,4 @@
 
 console.log('From the Console: Hello, javaScriptCartographer!');
 
-alert('Hello, javaScriptCartographer!');
+alert('Hello, javaScriptCartographer!\nThis prints as soon as the page loads since it is not deferred and not in a function.');
