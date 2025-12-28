9baadc4f5ba26882c474cc62de857008d7ed30f7
MU-TH-ER <zeerob440@tutanota.com>
2025-12-28 16:36:05 -0500

    feat: addd code space content.

--- template-literal/tLiteral.js
+++ template-literal/tLiteral.js
@@ -9,7 +9,6 @@ function dale()
 /*Template literals are like Python f-strings, but f' is replaced by ``, and {} is replaced
 by ${}*/
     let popup = `The best city on the planet is ${bestCity}, dale!`;
-
     console.log(popup); // outputs to console.
     alert(popup); // outputs via popup to user.
     }
