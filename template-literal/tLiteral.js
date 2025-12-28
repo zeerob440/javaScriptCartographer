f28f9cd1c33a5520a6de6f3ac8f06b0166aee98c
MU-TH-ER <zeerob440@tutanota.com>
2025-12-28 16:23:24 -0500

    feat: tLiteral.js is mostly a function now.

--- template-literal/tLiteral.js
+++ template-literal/tLiteral.js
@@ -1,10 +1,15 @@
 // a short program that demonstrates a Template Literal
 
-let bestCity = 'Miami';
+document.getElementById("t_string").addEventListener("click", dale)
+
+function dale() 
+    {
+    let bestCity = 'Miami';
 
 /*Template literals are like Python f-strings, but f' is replaced by ``, and {} is replaced
 by ${}*/
-let popup = `The best city on the planet is ${bestCity}, dale!`;
+    let popup = `The best city on the planet is ${bestCity}, dale!`;
 
-console.log(popup); // outputs to console.
-alert(popup); // outputs via popup to user.
+    console.log(popup); // outputs to console.
+    alert(popup); // outputs via popup to user.
+    }
