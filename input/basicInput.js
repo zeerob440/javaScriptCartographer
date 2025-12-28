79a451237f8e4c3cd88ae097e31e98ad4dbf1c4f
MU-TH-ER <zeerob440@tutanota.com>
2025-12-28 08:23:33 -0500

    update: usr_num updated to usr_letter. I want to introduce letters as first input to explain that inputs always start as strings untill converted.

--- input/basicInput.js
+++ input/basicInput.js
@@ -1,5 +1,5 @@
 //This program introduces basic input with prompt
 
-let usr_num =prompt("Please enter a number: ");
+let usr_letter =prompt("Please enter a letter: ");
 
-alert(`You entered: ${usr_num}.`)
+alert(`You entered: ${usr_letter}.`)
