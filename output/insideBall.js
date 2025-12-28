06969eb97d965796df1c7dbbc2f245756e65b920
MU-TH-ER <zeerob440@tutanota.com>
2025-12-28 17:59:21 -0500

    feat: created insideBall.js

---
+++ output/insideBall.js
@@ -0,0 +1,10 @@
+//A simple innerHtml program.
+document.getElementById("insideBallBTN").addEventListener("click",youreOutside);
+
+function youreOutside()
+    {document.getElementById("ifYoureNotInside").innerHTML
+        `<p>The point is, ladies and gentlemen,</p>
+        <p>Greed, for lack of a better word, is good.<p><br>
+        Wall Street 1987
+        `
+    }
