20c1856b02392890cfba66f78cdacab0610ea493
zeerob440 <zeerob440@tutanota.com>
2025-12-30 16:16:10 -0500

    Feat: added documentation to insideBall.js. Added codespaces to output/index.html

--- output/insideBall.js
+++ output/insideBall.js
@@ -2,7 +2,8 @@
 document.getElementById("insideBallBTN").addEventListener("click",youreOutside);
 
 function youreOutside()
-        {document.getElementById("ifYoureNotInside").innerHTML = 
+        {document.getElementById("ifYoureNotInside").innerHTML = // multiple line strings must be between ``
+        //once innerHTML is invoked, output is written in HTML>
         `<p>
             The point is, ladies and gentlemen,<br>
             greed -for lack of a better word- is good.
