89d8fa987b679d34d7268451d5ab3f56e3d9fa22
zeerob440 <zeerob440@tutanota.com>
2025-12-30 15:55:28 -0500

    fixed: grammar errors in insideBall.js, and output page

--- output/insideBall.js
+++ output/insideBall.js
@@ -2,10 +2,10 @@
 document.getElementById("insideBallBTN").addEventListener("click",youreOutside);
 
 function youreOutside()
-    {document.getElementById("ifYoureNotInside").innerHTML =
+        {document.getElementById("ifYoureNotInside").innerHTML = 
         `<p>
             The point is, ladies and gentlemen,<br>
-            Greed -for lack of a better word- is good.
+            greed -for lack of a better word- is good.
         </p>
         <p>
             Wall Street, 1987
